01345bc85499d5a0c530ac85453372072360e25b
saityagci <saityagci@gmail.com>
2023-01-05 19:39:15 -0500

    Json annotations added

--- src/main/java/com/cydeo/entity/ResponseWrapper.java
+++ src/main/java/com/cydeo/entity/ResponseWrapper.java
@@ -9,7 +9,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 @Setter
 @NoArgsConstructor
-
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ResponseWrapper {
 
     private boolean success;
