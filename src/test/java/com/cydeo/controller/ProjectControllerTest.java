85fa8c2715d20a28e5124aae10cccae56a8f00cb
saityagci <saityagci@gmail.com>
2023-01-19 18:31:53 -0500

    testing

--- src/test/java/com/cydeo/controller/ProjectControllerTest.java
+++ src/test/java/com/cydeo/controller/ProjectControllerTest.java
@@ -13,6 +13,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.time.LocalDate;
 
@@ -31,7 +32,7 @@ class ProjectControllerTest {
 
     @BeforeAll
     static void setup(){
-        token="Bearer eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJIdTlVXzQtOVZHZEl2b0Q2V3Z0M2UwMlZIMFBmNXNwZ3dJS0xJQkNOcHo0In0.eyJleHAiOjE2NzQxNjYxMjMsImlhdCI6MTY3NDE2NTgyMywianRpIjoiMjBiNDdjOTYtY2YzNS00MTgzLWIzMWYtNDdkMjZiZDY3MTdlIiwiaXNzIjoiaHR0cDovL2xvY2FsaG9zdDo4MDgwL2F1dGgvcmVhbG1zL2N5ZGVvLWRldiIsImF1ZCI6ImFjY291bnQiLCJzdWIiOiIzNzY5MGJlZi0xNzk5LTQ5N2QtYmQ2ZC0wOWJhOGJkYWJkZDQiLCJ0eXAiOiJCZWFyZXIiLCJhenAiOiJ0aWNrZXRpbmctYXBwIiwic2Vzc2lvbl9zdGF0ZSI6IjhiNzg5M2E2LTg2NTMtNDcxMC04ZjQ2LTA5M2E5MDA1OTgzZiIsImFjciI6IjEiLCJhbGxvd2VkLW9yaWdpbnMiOlsiaHR0cDovL2xvY2FsaG9zdDo4MDgxIl0sInJlYWxtX2FjY2VzcyI6eyJyb2xlcyI6WyJvZmZsaW5lX2FjY2VzcyIsImRlZmF1bHQtcm9sZXMtY3lkZW8tZGV2IiwidW1hX2F1dGhvcml6YXRpb24iXX0sInJlc291cmNlX2FjY2VzcyI6eyJ0aWNrZXRpbmctYXBwIjp7InJvbGVzIjpbIkFkbWluIl19LCJhY2NvdW50Ijp7InJvbGVzIjpbIm1hbmFnZS1hY2NvdW50IiwibWFuYWdlLWFjY291bnQtbGlua3MiLCJ2aWV3LXByb2ZpbGUiXX19LCJzY29wZSI6Im9wZW5pZCBlbWFpbCBwcm9maWxlIiwic2lkIjoiOGI3ODkzYTYtODY1My00NzEwLThmNDYtMDkzYTkwMDU5ODNmIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsIm5hbWUiOiJvenp5IG96enkiLCJwcmVmZXJyZWRfdXNlcm5hbWUiOiJvenp5IiwiZ2l2ZW5fbmFtZSI6Im96enkiLCJmYW1pbHlfbmFtZSI6Im96enkifQ.kU_Up_mrzw92CDQpxRG6ahWSeywSiNl3aYBWGERrBLsUYBC63FxpBFUmbDzwWawthvousNmSJPYX8_hCcbz5ofhHMvtMzpq68tq74pUuimQovAJeLIOBbmbXF_9Fe_auLq1-n31-CPPbpYAVw0uxtvh4BlPhBhJkJCdOUlbmrWszgv29_S4uOdawQyHkjUznc5TSQBAp11kKKdCwKMiQmgZMUAmWnc4iE0SZFOuO-h8AkBBGTfH3Gguyec6a9v5UAW3ib9g1ncerr_ptrXebrNbfZMEV4GqxLs2Sc00mixwVCKQ_A_7mD9Yh2M2YJZgwL6KmZ8ktikZJQKS44VOlDA";
+        token="Bearer eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJIdTlVXzQtOVZHZEl2b0Q2V3Z0M2UwMlZIMFBmNXNwZ3dJS0xJQkNOcHo0In0.eyJleHAiOjE2NzQxNzEzMDksImlhdCI6MTY3NDE3MTAwOSwianRpIjoiN2ZlYTdlOWMtODhlOC00OWQxLWE0MWUtNWRkMmY0YTdhNmI1IiwiaXNzIjoiaHR0cDovL2xvY2FsaG9zdDo4MDgwL2F1dGgvcmVhbG1zL2N5ZGVvLWRldiIsImF1ZCI6ImFjY291bnQiLCJzdWIiOiI2OWFlMzM2Zi0wOGQ0LTQyMzctYmM5Mi1jMmI2OTY2MzkyMDMiLCJ0eXAiOiJCZWFyZXIiLCJhenAiOiJ0aWNrZXRpbmctYXBwIiwic2Vzc2lvbl9zdGF0ZSI6IjY1OWMzMzYxLTBlYzgtNDlmYS04NzczLTM0OThhMTIyZTg1ZSIsImFjciI6IjEiLCJhbGxvd2VkLW9yaWdpbnMiOlsiaHR0cDovL2xvY2FsaG9zdDo4MDgxIl0sInJlYWxtX2FjY2VzcyI6eyJyb2xlcyI6WyJvZmZsaW5lX2FjY2VzcyIsImRlZmF1bHQtcm9sZXMtY3lkZW8tZGV2IiwidW1hX2F1dGhvcml6YXRpb24iXX0sInJlc291cmNlX2FjY2VzcyI6eyJ0aWNrZXRpbmctYXBwIjp7InJvbGVzIjpbIk1hbmFnZXIiXX0sImFjY291bnQiOnsicm9sZXMiOlsibWFuYWdlLWFjY291bnQiLCJtYW5hZ2UtYWNjb3VudC1saW5rcyIsInZpZXctcHJvZmlsZSJdfX0sInNjb3BlIjoib3BlbmlkIGVtYWlsIHByb2ZpbGUiLCJzaWQiOiI2NTljMzM2MS0wZWM4LTQ5ZmEtODc3My0zNDk4YTEyMmU4NWUiLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwibmFtZSI6Im1pa2Ugc21pdGgiLCJwcmVmZXJyZWRfdXNlcm5hbWUiOiJtaWtlIiwiZ2l2ZW5fbmFtZSI6Im1pa2UiLCJmYW1pbHlfbmFtZSI6InNtaXRoIn0.igzAHA3p_nTtRktFz_RfGmIiVmS-brRQLSJaLAyFssk7uWGJU3ZozudF9LAYRiLoFk6FUVFHOaeJjWZkjuSBhFrsgo9YuVS3fUZpU-HH0D64MLDp1r2tGh6eereWKlUSaWgJeQhEpFBUdWCBN6W04I6rv-DtuylYtBhjp0wqOvpxBkln1CvRCP91DJdAsqiKDj9Waxdx_L05l6gEYihqvIh-y4QZol7FM8DjnGJLTF_rLowQ0a81ZMz8DPZF3_fUHq_mplCA5trUIEVXOtMaxTuVt8NlyadxIqLdsvZ94ZWwD_aubiIvlDZpC8MaPsiHgF7n1l2exgy_Q_VJnNXJdQ";
         userDTO= UserDTO.builder()
                 .id(2L)
                 .firstName("ozzy")
@@ -64,7 +65,12 @@ class ProjectControllerTest {
                 .get("/api/v1/project")
                 .header("Authorization",token)
                 .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data[0].projectCode")
+                        .exists())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data[0].assignedManager.userName")
+                        .isNotEmpty());
+
 
     }
 
