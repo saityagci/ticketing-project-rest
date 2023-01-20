1d8c7612c1ee8ec07b68778eaa70ef0e0c72cd04
saityagci <saityagci@gmail.com>
2023-01-19 19:39:02 -0500

    testing

--- src/test/java/com/cydeo/controller/ProjectControllerTest.java
+++ src/test/java/com/cydeo/controller/ProjectControllerTest.java
@@ -9,6 +9,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -34,13 +35,13 @@ class ProjectControllerTest {
     static String token;
 
     @BeforeAll
-    static void setup(){
+     static void setup(){
         token="Bearer eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJIdTlVXzQtOVZHZEl2b0Q2V3Z0M2UwMlZIMFBmNXNwZ3dJS0xJQkNOcHo0In0.eyJleHAiOjE2NzQyMDg3NjEsImlhdCI6MTY3NDE3Mjc2MSwianRpIjoiNWViYzUzOTUtNTZjNS00YzM1LTg0ZDMtZGU1MjVjYmQwNTc4IiwiaXNzIjoiaHR0cDovL2xvY2FsaG9zdDo4MDgwL2F1dGgvcmVhbG1zL2N5ZGVvLWRldiIsImF1ZCI6ImFjY291bnQiLCJzdWIiOiI2OWFlMzM2Zi0wOGQ0LTQyMzctYmM5Mi1jMmI2OTY2MzkyMDMiLCJ0eXAiOiJCZWFyZXIiLCJhenAiOiJ0aWNrZXRpbmctYXBwIiwic2Vzc2lvbl9zdGF0ZSI6Ijg2ZDc2MzM1LWM1MWItNGMzZS1hNmMyLTcwYTNkYjY4NmFjZiIsImFjciI6IjEiLCJhbGxvd2VkLW9yaWdpbnMiOlsiaHR0cDovL2xvY2FsaG9zdDo4MDgxIl0sInJlYWxtX2FjY2VzcyI6eyJyb2xlcyI6WyJvZmZsaW5lX2FjY2VzcyIsImRlZmF1bHQtcm9sZXMtY3lkZW8tZGV2IiwidW1hX2F1dGhvcml6YXRpb24iXX0sInJlc291cmNlX2FjY2VzcyI6eyJ0aWNrZXRpbmctYXBwIjp7InJvbGVzIjpbIk1hbmFnZXIiXX0sImFjY291bnQiOnsicm9sZXMiOlsibWFuYWdlLWFjY291bnQiLCJtYW5hZ2UtYWNjb3VudC1saW5rcyIsInZpZXctcHJvZmlsZSJdfX0sInNjb3BlIjoib3BlbmlkIGVtYWlsIHByb2ZpbGUiLCJzaWQiOiI4NmQ3NjMzNS1jNTFiLTRjM2UtYTZjMi03MGEzZGI2ODZhY2YiLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwibmFtZSI6Im1pa2Ugc21pdGgiLCJwcmVmZXJyZWRfdXNlcm5hbWUiOiJtaWtlIiwiZ2l2ZW5fbmFtZSI6Im1pa2UiLCJmYW1pbHlfbmFtZSI6InNtaXRoIn0.IWE_GsACWuM9Kz1xohqcNFgTp_6TSUuwTjoczil1q0-nrWAFN3ArN0Lrtm5zozIZzUY0OQ6H65dxV5f78wq8yfgNB9YRJdUoG9erEDKnL8CiTiTIt86EL-7t6RxxSLo-hKKNv2f0k3RmK8uY0STmWZf2A0kRcL27SyOIYAeGJgXnFWCUM9zCCfPPEVnAytOQl-qizEKUDdBD3yZQl7UNSN22JqZuK6ImlhPyk6jqtgjcIZfeq6b4wcwOvFCnJnzfLRSVRXPG1TA9WtrJ7Xu-_PaxQ2AF7GYzdBnNXKarf2SCV4b-NVqAdhY-q-lnjiZ4uJAf9TCyGbNl3_yajstmHw";
         userDTO= UserDTO.builder()
                 .id(2L)
-                .firstName("ozzy")
-                .lastName("ozzy")
-                .userName("ozzy")
+                .firstName("mike")
+                .lastName("smith")
+                .userName("mike")
                 .passWord("abc1")
                 .confirmPassWord("abc1")
                 .role(new RoleDTO(2L,"Manager"))
@@ -55,6 +56,7 @@ class ProjectControllerTest {
                 .projectDetail("Api test")
                 .projectStatus(Status.OPEN)
                 .build();
+
     }
     @Test
     public void givenNoToken_whenGetRequest() throws Exception{
